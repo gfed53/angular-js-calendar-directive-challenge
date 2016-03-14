26993ddb0fa387c0744a5122a4610ad6bce83507
gfed53 <gfederico7153@gmail.com>
2016-03-14 16:07:40 -0400

    Initial commit. Dry test for data acquisition

--- app/app.js
+++ app/app.js
@@ -1,3 +1,16 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+.directive('myCalendar', function(){
+	return {
+		restrict: "E",
+		transclude: true,
+		templateUrl: "template.html",
+		scope: true,
+		controller: function($scope, $element, $attrs){
+			var testDate = new Date();
+			var testCalendar = CalendarRange.getMonthlyRange(testDate);
+			console.log(testCalendar.days);
+		}
+	}
+})
