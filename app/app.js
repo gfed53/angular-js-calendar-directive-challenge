b17cafd12027aab90308a32fbf6eff290cca61ae
gfed53 <gfederico7153@gmail.com>
2016-03-15 17:10:37 -0400

    Added form, controller with some tests

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+.controller('SelectController', SelectController)
 .directive('myCalendar', function(){
 	return {
 		restrict: "E",
@@ -9,9 +10,26 @@ angular.module('calendarDemoApp', [])
 		scope: true,
 		controller: function($scope, $element, $attrs){
 			var testDate = new Date();
+			console.log(testDate);
 			var testCalendar = CalendarRange.getMonthlyRange(testDate);
+			console.log(testCalendar);
 			console.log(testCalendar.days);
 			$scope.days = testCalendar.days;
 		}
 	}
 })
+
+function SelectController() {
+	var vm = this;
+	vm.submit = submit;
+
+	function submit(){
+		console.log(dateConstruct());
+	}
+
+	function dateConstruct(){
+		var date = vm.month + " " + vm.year;
+		// console.log(date);
+		return date;
+	}
+};
