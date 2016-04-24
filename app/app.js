6d8cb46d6f5fa6ded8c820649ee7c3f2d9af742e
gfed53 <gfederico7153@gmail.com>
2016-04-24 16:30:58 -0400

    Changes to positioning, code cleanup

--- app/app.js
+++ app/app.js
@@ -1,18 +1,11 @@
 angular.module('calendarDemoApp', [])
 
-// your controller and directive code go here
 .controller('SelectController', ['ALL_MONTHS', SelectController])
 .directive('myCalendar', function(){
 	return {
 		restrict: "E",
 		templateUrl: "template.html",
 		scope: true,
-		link: function(scope, element, attrs){
-
-			console.log(scope); 
-			// scope.days = vm.days;
-			// console.log(scope.days);
-		}
 	}
 })
 
@@ -25,21 +18,16 @@ function SelectController(ALL_MONTHS) {
 	vm.start = vm.range.start;
 	vm.needStyling = needStyling;
 	vm.monthChoices = ALL_MONTHS;
-	console.log(vm);
 
 	function submit(){
-		// console.log(dateConstruct());
 		var date = dateConstruct();
-		console.log(date);
 		vm.range = CalendarRange.getMonthlyRange(date);
-		console.log(vm.range);
 		vm.days = vm.range.days;		
 	}
 
 	function dateConstruct(){
 		var dateString = vm.year + " " + vm.month + " 01",
 		date = moment(dateString, "YYYY MMM DD")._d;
-		console.log(date);
 		return date;
 	}
 
